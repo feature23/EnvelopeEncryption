f84e035efebbe9eb10fe83c583131585feb91205
James Blair <james.blair@outlook.com>
2022-11-15 11:19:58 -0700

    Add optional DefaultAzureCredentialOptions parameter to CryptographyClientFactory methods.

--- F23.EnvelopeEncryption.KeyVault/CryptographyClientFactory.cs
+++ F23.EnvelopeEncryption.KeyVault/CryptographyClientFactory.cs
@@ -12,15 +12,17 @@ public class CryptographyClientFactory
     /// Creates a new <see cref="CryptographyClient"/> from the given key <paramref name="uri"/>.
     /// </summary>
     /// <param name="uri">The URI of the key.</param>
+    /// /// <param name="credentialOptions">Optional <see cref="DefaultAzureCredentialOptions"/> to customize the authentication process.</param>
     /// <returns>Returns a new <see cref="CryptographyClient"/> instance.</returns>
-    public CryptographyClient CreateFromKeyUri(string uri)
-        => CreateFromKeyUri(new Uri(uri));
-    
+    public CryptographyClient CreateFromKeyUri(string uri, DefaultAzureCredentialOptions? credentialOptions = null)
+        => CreateFromKeyUri(new Uri(uri), credentialOptions);
+
     /// <summary>
     /// Creates a new <see cref="CryptographyClient"/> from the given key <paramref name="uri"/>.
     /// </summary>
     /// <param name="uri">The URI of the key.</param>
+    /// <param name="credentialOptions">Optional <see cref="DefaultAzureCredentialOptions"/> to customize the authentication process.</param>
     /// <returns>Returns a new <see cref="CryptographyClient"/> instance.</returns>
-    public CryptographyClient CreateFromKeyUri(Uri uri)
-        => new(uri, new DefaultAzureCredential());
+    public CryptographyClient CreateFromKeyUri(Uri uri, DefaultAzureCredentialOptions? credentialOptions = null)
+        => new(uri, new DefaultAzureCredential(credentialOptions));
 }
